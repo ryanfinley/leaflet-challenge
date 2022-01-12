c3e6a7b4db04ae6fa42da4569ea8000cd095102a
ryanfinley <ryanfinley.k@outlook.com>
2022-01-12 12:32:13 -0500

    commit

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,6 +5,16 @@ var defaultMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
 });
 
 
+// grayscale layer
+var grayscale = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+});
+
+
 // make a map object
 var myMap = L.map("map", {
     center: [36.7783, -119.4179],
