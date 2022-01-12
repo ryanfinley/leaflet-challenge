52752ff3629f05b35cb5fc55073c9a6e26ba78b9
ryanfinley <ryanfinley.k@outlook.com>
2022-01-12 13:38:55 -0500

    commit

--- static/js/logic.js
+++ static/js/logic.js
@@ -14,12 +14,28 @@ var grayscale = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}
 	ext: 'png'
 });
 
+// water color layer
+var waterColor = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+// make a basemaps object
+let basemaps = {
+    Default: defaultMap,
+    GrayScale: grayscale,
+	"Water Color": waterColor
+};
 
 // make a map object
 var myMap = L.map("map", {
     center: [36.7783, -119.4179],
-    zoom: 3
+    zoom: 3,
+	layers: [defaultMap, grayscale, waterColor]
 });
 
 // add the default map to the map
-defaultMap.addto(myMap);
+defaultMap.addTo(myMap);
+
+// add the Layer control
+L.control.layers(basemaps).addTo(myMap);
